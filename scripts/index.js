e82a23737e0b3769bdc6cd7b196622aaeac88e4f
Hor4iK <kiasdog@mail.ru>
2023-12-05 20:28:20 +0300

    Final version project

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,27 @@
 // @todo: Темплейт карточки
+const cardList = document.querySelector('.places__list');
+
+const deleteCard = e => {
+  const deletedItem = e.target.closest('.places__item');
+  deletedItem.remove();
+}
+
+function addCards(linkValue, descriptionValue) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+
+  cardElement.querySelector('.card__image').src = linkValue;
+  cardElement.querySelector('.card__image').alt = descriptionValue;
+  cardElement.querySelector('.card__description').textContent = descriptionValue;
+  deleteButton.addEventListener('click', e => {
+    deleteCard(e);
+  });
+
+  cardList.append(cardElement);
+};
+
+initialCards.forEach(item => {addCards(item.link, item.name)});
 
 // @todo: DOM узлы
 
